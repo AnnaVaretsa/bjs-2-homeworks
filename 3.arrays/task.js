f01e6c576ae17bb02c73d5e4c16539f7b486cf0d
Anna Varetsa <besssik@gmail.com>
2022-07-13 19:55:35 +0300

    Расширенная работа с массивами

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (Array.isArray(arr1) && Array.isArray(arr2) ) {   
+    result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+    console.log(`Сравнение массивов размером ${arr1.length} и ${arr2.length} `)  } 
+    else {    
+      console.log("Некорректные входные данные");
+    }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(el => el > -1).filter(el => el % 3 === 0).map(el => el *10)
   return resultArr; // array
 }
