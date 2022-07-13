baa11423e5dd8ad03f654c986c2f6e68ba17a3ff
Anna Varetsa <besssik@gmail.com>
2022-07-13 23:22:06 +0300

    Задание к занятию "Объекты"

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
-function Student(name, gender, age) {
-    // Ваш код
+function Student (name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
 
-}
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+      var arr = [];
+      arr.unshift(mark);
+      this.marks = arr;
+    } 
+    else {
+      this.marks.splice(this.marks.length, 0, mark);
+    }
+  }
 
-// ваш код для остальных методов
+  Student.prototype.addMarks = function (...theMarks ) {
+    var arr = [];
+    for (const arg of theMarks) {
+      arr.push(arg);
+    }
+    this.marks = arr;
+    }
+
+  Student.prototype.getAverage = function () {
+    let avg = this.marks.reduce((a, b) => a + b, 0) / this.marks.length;
+    return avg;
+  }
+
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
